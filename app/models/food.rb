98ec24b8361e93217fc18d3258809c75720f56ca
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-08-04 15:03:35 +0500

    complete public recipe

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipes, through: :recipe_foods
   has_many :recipe_foods
-
+  
   # def price=(value)
   #   super(value&.delete('$'))
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :name, presence: true
 end
